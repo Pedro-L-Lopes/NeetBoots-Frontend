8cc988e6033862d855286ec0a2f1a7416eb46bd4
Pedro Lucas <pedrolucas122737@gmail.com>
2023-10-21 11:04:24 -0300

    Navbar para clientes autenticados

--- src/components/NavAndFooter/Navbar0.jsx
+++ src/components/NavAndFooter/Navbar0.jsx
@@ -1,16 +1,45 @@
-import { Link } from "react-router-dom";
-
 import "./Navbar.css";
 
+// Components
+import { Link, NavLink } from "react-router-dom";
+
+// Hooks
+import { useState } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 const Navbar0 = () => {
+  const { auth } = useAuth();
+  const { client } = useSelector((state) => state.authClient);
+
   return (
     <nav className="navbar0">
       <div className="titulo">
         <h1>O melhor está aqui</h1>
       </div>
-      <div className="titulo2">
-        Ajuda | Junte-se a nós | <Link to="/login">Entrar</Link>
-      </div>
+      {auth ? (
+        <>
+          <div className="titulo1">
+            <Link to="/">Ajuda</Link>
+            {client && (
+              <p>
+                <NavLink to={`/clients/${client.id}`}>Minha conta</NavLink>
+              </p>
+            )}
+            <p>
+              <NavLink to={`/profile`}>Profile</NavLink>
+            </p>
+            <p>Sair</p>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="titulo2">
+            Ajuda | Junte-se a nós | <Link to="/login">Entrar</Link>
+          </div>
+        </>
+      )}
     </nav>
   );
 };
